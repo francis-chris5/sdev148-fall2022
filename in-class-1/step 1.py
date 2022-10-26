302754af6341af82422aff9ee68d14f4b20c1aa5
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2022-10-26 17:28:30 -0400

    Add files via upload

---
+++ in-class-1/step 1.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 14:52:30 2022
+
+@author: c.s.francis
+"""
+
+
+import pygame
+import random
+
+
+
+## initialize, start, begin, creation....
+
+
+pygame.init()
+clock = pygame.time.Clock()
+pygame.key.set_repeat(10, 10)
+
+score = 0
+pygame.font.init()
+scoreFont = pygame.font.SysFont("Times New Roman", 23)
+
+
+windowSize = (960, 480)
+scene = pygame.display.set_mode(windowSize)
+
+playerSpeed = 3
+playerX = 123
+playerY = 123
+playerSprite = pygame.image.load("character.png")
+playerBounds = playerSprite.get_rect()
+playerBounds.x = playerX
+playerBounds.y = playerY
+
+backgroundSprite = pygame.image.load("background.png")
+
+
+coinX = 567
+coinY = 234
+coinSprite = pygame.image.load("coin.png")
+coinBounds = coinSprite.get_rect()
+coinBounds.x = coinX
+coinBounds.y = coinY
+
+
+
+
+
+## main loop, frame update, next...
+
+playing = True
+while playing:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                playerX -= playerSpeed
+            if event.key == pygame.K_w:
+                playerY -= playerSpeed
+            if event.key == pygame.K_s:
+                playerY += playerSpeed
+            if event.key == pygame.K_d:
+                playerX += playerSpeed
+            
+           
+    if playerBounds.colliderect(coinBounds):
+        coinX = random.randint(40, 920)
+        coinY = random.randint(40, 440)
+        score += 1 
+    
+
+
+    scene.blit(backgroundSprite, (0, 0))
+    
+    scene.blit(playerSprite, (playerX, playerY))
+    playerBounds.x = playerX
+    playerBounds.y = playerY
+    
+    scene.blit(coinSprite, (coinX, coinY))
+    coinBounds.x = coinX
+    coinBounds.y = coinY
+    
+    showScore = scoreFont.render("COINS: " + str(score), False, (255, 255, 255))
+    scene.blit(showScore, (700, 45))
+    
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############## white space for scrolling
+
+
+
+
+
+
+
+
